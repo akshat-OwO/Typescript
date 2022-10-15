3ac02e88a8fd307c73bde76a3cfe6da0b58b7d4f
AKSHAT-SINGH-DEV <AKSHATSINGHDELHI@GMAIL.COM>
2022-10-15 15:02:54 +0530

    Public, Private & Readonly

--- public/app.js
+++ public/app.js
@@ -1,4 +1,26 @@
 "use strict";
+// classes
+class Invoice {
+    // readonly client: string;
+    // private details: string;
+    // amount: number;
+    constructor(client, details, amount) {
+        this.client = client;
+        this.details = details;
+        this.amount = amount;
+    }
+    format() {
+        return `${this.client} owes $${this.amount} for ${this.details}`;
+    }
+}
+const invOne = new Invoice('mario', 'work on the mario website', 250);
+const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
+let invoices = [];
+invoices.push(invOne);
+invoices.push(invTwo);
+invoices.forEach(inv => {
+    console.log(inv.client, inv.amount, inv.format());
+});
 const form = document.querySelector('.new-item-form');
 console.log(form.children);
 const type = document.querySelector('#type');
--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,31 @@
+// classes
+class Invoice {
+    // readonly client: string;
+    // private details: string;
+    // amount: number;
+
+    constructor(
+        readonly client: string,
+        private details: string,
+        public amount: number
+    ) {}
+
+    format() {
+        return `${this.client} owes $${this.amount} for ${this.details}`;
+    }
+}
+
+const invOne = new Invoice('mario', 'work on the mario website', 250);
+const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
+
+let invoices: Invoice[] = [];
+invoices.push(invOne);
+invoices.push(invTwo);
+
+invoices.forEach(inv => {
+    console.log(inv.client, inv.amount, inv.format());
+});
+
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
 console.log(form.children);
 
